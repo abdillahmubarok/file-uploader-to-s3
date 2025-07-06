849dfc3c430da880c28fbd719149a314a04e9f19
Muhammad Abdillah Mubarok (Mazz Jack) <muhammadmubarok71@gmail.com>
2025-07-06 10:31:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -13,7 +13,7 @@ export function MainNav() {
         <div className="container mx-auto flex h-16 items-center px-4">
             <Link href="/" className="mr-6 flex items-center space-x-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6"><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"/><path d="M12 12v9"/><path d="m16 16-4-4-4 4"/></svg>
-                <span className="font-bold">Pakde Dosen Storage</span>
+                <span className="font-bold">Pakde Dosen</span>
             </Link>
             <div className="flex items-center gap-4">
                 <Link
